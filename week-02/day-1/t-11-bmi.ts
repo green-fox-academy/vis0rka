1070945ae64d2d1636d4581160d0c974161ff824
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-29 11:28:07 +0100

    task-11 BMI Index

---
+++ week-02/day-1/t-11-bmi.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+let BMI: number = null;
+
+BMI = massInKg/heightInM**2
+
+// Print the Body mass index (BMI) based on these values
+
+console.log("the Body mass index: " + BMI.toFixed(2));
