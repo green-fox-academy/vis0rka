853da83d9d292b7f8de77699a70be6b6857569f2
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-29 10:43:46 +0100

    task-05 different types

---
+++ week-02/day-1/t-05-different-types.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Prints a string to the terminal window
+console.log('Hello world!');
+
+// Prints an integer to the terminal window
+console.log(42);
+
+// Prints a floating point number to the terminal window
+console.log(3.141592);
+
+// Prints a boolean to the terminal window
+console.log(true);
