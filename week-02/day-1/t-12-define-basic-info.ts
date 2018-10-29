27d75bbc9b05c627d5df163fa8783fbdb13c2b9e
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-29 11:36:03 +0100

    task-12 BMI define basic info

---
+++ week-02/day-1/t-12-define-basic-info.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let myName: string = "Barta Viktor";
+let age: number = 31;
+let height: number = 1.82;
+let married: boolean = false;
+
+console.log("My name is: " + myName);
+console.log("My age is: " + age);
+console.log("My height is: " + height);
+console.log("Am I married: " + married);
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
