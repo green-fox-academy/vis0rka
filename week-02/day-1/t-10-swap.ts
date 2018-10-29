2770d93fe70252517985c0541f52b514e746633e
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-29 11:23:01 +0100

    task-10 swap-numbers

---
+++ week-02/day-1/t-10-swap.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+let c: number = null;
+
+c = a;
+a = b;
+b = c;
+
+
+console.log(a);
+console.log(b);
