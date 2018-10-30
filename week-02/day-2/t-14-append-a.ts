935ac578fb558707285cb51c505da8811cc5b4f3
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-30 14:06:36 +0100

    t-14-append-a

---
+++ week-02/day-2/t-14-append-a.ts
@@ -0,0 +1,12 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal","pand","zebr"]
+
+animals = animals.map(function(e) {
+    return e = e +"a";
+}
+
+)
+console.log(animals);
