97e4fbe15fd5f75b1e4eb12ca015dc6f6a1bdf83
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-30 10:56:06 +0100

    task-04 sum numbers

---
+++ week-02/day-2/t-04-sum.ts
@@ -0,0 +1,13 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+let x: number = 5;
+let y: number = 5;
+let z: number = 5;
+
+let sumNumber: number;
+
+function sum(sumNumber) {
+    return x+y+z;
+}
+console.log(sum(sumNumber));
