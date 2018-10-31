6088b8ba17a9a9e8eb0f977226b23053910994a2
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-10-31 13:49:05 +0100

    ts-21-bubble numbers as parameter

---
+++ week-02/day-3/ts-21-bubble.ts
@@ -0,0 +1,30 @@
+//  Create a function that takes a list of numbers as parameter
+//  Returns a list where the elements are sorted in ascending numerical order
+//  Make a second boolean parameter, if it's `true` sort that list descending
+
+
+function bubble(numbers: number[]) {
+    numbers.sort(function (a, b) {
+        return a - b;
+    })
+    return numbers;  
+}
+function advancedBubble(numbers: number[], order: boolean) {
+    if (order == true) {
+        numbers.sort(function (a, b) {
+            return b - a;
+        })
+        return numbers;
+    } else {
+        numbers.sort(function (a, b) {
+            return a - b;
+        })
+        return numbers;
+    }
+}
+
+//  Example:
+console.log(bubble([34, 12, 24, 9, 5]));
+//  should print [5, 9, 12, 24, 34]
+console.log(advancedBubble([34, 12, 24, 9, 5], true));
+//  should print [34, 24, 12, 9, 5]
