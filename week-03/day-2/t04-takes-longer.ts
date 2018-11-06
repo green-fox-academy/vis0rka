84fd4534c3735353fda068c5357de691f966c25e
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-11-06 12:59:25 +0100

    t04-takes-longer with 2 solution

--- week-03/day-2/t04-takes-longer.ts
+++ week-03/day-2/t04-takes-longer.ts
@@ -3,9 +3,15 @@
 // Add "always takes longer than" to between the words "It" and "you"
 
 let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+/* solution1 */
+/* quote = quote.replace("It you", "It always takes longer than you");  */
 
-quote = quote.replace("It you", "It always takes longer than you"); 
+/* solution2 */
 
+let quote1 = quote.substr(0,20);
+let quote3 = quote.substr(20,99);
+let quote2 = " always takes longer than";
 
+quote = quote1 + quote2 + quote3;
 
 console.log(quote);
