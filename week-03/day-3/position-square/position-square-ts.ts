6de15b65650192bc87c9bc623b11d9da2985085c
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-11-07 11:45:51 +0100

    position-square

---
+++ week-03/day-3/position-square/position-square-ts.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a square drawing function that takes 2 parameters:
+// The x and y coordinates of the square's top left corner
+// and draws a 50x50 square from that point.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+for (let i = 0; i < 3; i++) {   
+    drawSquareCenter(i*50,i*50);
+}
+
+function drawSquareCenter (x,y) {
+    ctx.beginPath();
+    ctx.fillRect(0+x, 0, 50, 50);
+
+}
