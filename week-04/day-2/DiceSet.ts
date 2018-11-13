f0514d97e4f1b2cc59bb67567b3038b1a588933b
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-11-13 17:19:29 +0100

    task08 DiceSet

---
+++ week-04/day-2/DiceSet.ts
@@ -0,0 +1,60 @@
+class DiceSet {
+  // You have a `DiceSet` class which has a list for 6 dices
+  // You can roll all of them with roll()
+  // Check the current rolled numbers with getCurrent()
+  // You can reroll with reroll()
+  // Your task is to roll the dices until all of the dices are 6
+      dices: number[];
+      readonly numOfDices: number = 6;
+  
+      roll(): number[] {
+          this.dices = [];
+          for(var i = 0; i < this.numOfDices; i++) { 
+              this.dices.push(Math.floor(Math.random() * 6 + 1));
+          }
+          return this.dices;
+      }
+  
+      reroll(index?: number) {
+          if(index == undefined) {
+              for(var i = 0; i < this.numOfDices; i++) { 
+                  this.dices[i] = Math.floor(Math.random() * 6 + 1);
+              }
+          } else {
+              this.dices[index] = Math.floor(Math.random() * 6 + 1);
+          }
+      }
+      
+      getCurrent(index?: number) {
+          if(index == undefined) {
+              for(var i = 0; i < this.numOfDices; i++) { 
+                  console.log(this.dices[i]);
+              }
+          } else {
+              console.log(this.dices[index]);
+          }        
+      }
+
+  }
+  
+  let diceSet = new DiceSet();
+  diceSet.roll();
+  //diceSet.getCurrent();
+  console.log("------------------");
+ 
+
+ while(!diceSet.dices.every(e => e === 6)){
+  for (let i = 0; i < 6; i++) {
+    if (diceSet.dices[i] !== 6) {
+      diceSet.reroll(i);     
+    } 
+  }
+ }
+
+ diceSet.getCurrent();
+
+
+
+  
+  
+
