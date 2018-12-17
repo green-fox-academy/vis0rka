666c97a5781fdbf6a0cff2e1651914153c9f1cea
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-12-17 16:00:30 +0100

    feat(coctails)

---
+++ week-09/day-1/cocktails/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const cocktails = [
+  { name: 'GIN FIZZ', price: 1520, contains: ['gin', 'sugar', 'lemon juice', 'soda'], isAlcoholic: true },
+  { name: 'BLOODY MARY', price: 1650, contains: ['vodka', 'tomato juice', 'spices'], isAlcoholic: true },
+  { name: 'SEX ON THE BEACH', price: 1850, contains: ['vodka', 'peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: true },
+  { name: 'CUBA LIBRE', price: 1850, contains: ['rum', 'cola', 'lime juice'], isAlcoholic: true },
+  { name: 'MOJITO', price: 1850, contains: ['rum', 'sugar', 'lime juice', 'soda water'], isAlcoholic: true },
+  { name: 'LONG ISLAND ICE TEA', price: 2450, contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'], isAlcoholic: true },
+  { name: 'VIRGIN MOJITO', price: 990, contains: ['sugar', 'lime juice', 'soda water'], isAlcoholic: false },
+  { name: 'SAFE SEX ON THE BEACH', price: 990, contains: ['peach schnapps', 'orange juice', 'cranberry juice'], isAlcoholic: false },
+];
+
+const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// set static files
+app.use('/assets', express.static('assets'));
+
+
+
+// home page
+
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  const { alcohol } = req.query;
+  if (alcohol) {
+    res.render('home', {
+      alcoholList,
+      cocktails: cocktails.filter(el => el.contains.includes(alcohol)),
+    });
+
+  } else {
+    res.render('home', {
+      alcoholList,
+      cocktails
+    })
+  }
+});
+
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
