80d0e1979dd56003c125e657be947f9defe032ec
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-12-19 13:32:30 +0100

    feat(fronted) add endpoint array

--- week-09/day-3/frontend/app.js
+++ week-09/day-3/frontend/app.js
@@ -51,7 +51,18 @@ app.post('/dountil/:action', (req,res) => {
 })
 
 
-
+app.post('/array/', (req,res) => {
+  const what  = req.body.what;
+  if (what && req.body.numbers) {
+    if ( what == 'sum') {
+      res.json({'result': sum(req.body.numbers)})
+    } else if ( what == 'multiply') {
+      res.json({'result': multiply(req.body.numbers)})
+    } else if ( what == 'double') {
+      res.json({'result': double(req.body.numbers)})
+    }
+  } else res.json({ "error": "Please provide what to do with the numbers!" })
+})
 
 
 
