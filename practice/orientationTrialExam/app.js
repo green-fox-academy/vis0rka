6df7c6ecda2ba8bf4a86e321e6be91972c24469c
Viktor Barta <barta.viktor.ufs@gmail.com>
2018-12-31 10:18:50 +0100

    feat(URL shortening app) add delete endpoint

--- practice/orientationTrialExam/app.js
+++ practice/orientationTrialExam/app.js
@@ -54,13 +54,13 @@ app.post('/api/links', (req, res) => {
           return
         }
         conn.query(`SELECT * FROM urlStore WHERE alias = '${alias}';`, (err, info) => {
-           res.json({ 
+          res.json({
             id: info[0].id, // <- itt ezeket a [0] hívásokat valahogy ki lehet kerülni?
             url: info[0].url,
             alias: info[0].alias,
             hitCount: info[0].hitCount,
             secretCode: info[0].secretCode
-           });
+          });
         })
       });
     }
@@ -71,40 +71,71 @@ const randomCode = () => {
   return Math.floor(1000 + Math.random() * 9000);
 };
 
-app.get('/a', (req,res) => {
+app.get('/a', (req, res) => {
   const { alias } = req.query
 
-  conn.query(`SELECT * FROM urlStore;`, (err,rows) => {
+  conn.query(`SELECT * FROM urlStore;`, (err, rows) => {
     if (err) {
       console.log(err.message);
-      res.status(500).json({message: 'Internal server error'});
+      res.status(500).json({ message: 'Internal server error' });
       return
     }
-    if (rows.find(element => element.alias = alias)) {
+    if (rows.find(element => element.alias == alias)) {
       conn.query(`UPDATE urlStore SET hitCount = hitCount + 1 WHERE alias = '${alias}';`, (err, data) => {
         if (err) {
           console.log(err.message);
-          res.status(500).json({message: 'Internal server error'});
+          res.status(500).json({ message: 'Internal server error' });
           return
         }
-        res.json({message: `The alias:${alias} hitcount increment by 1`})
+        res.json({ message: `The alias:${alias} hitcount increment by 1` })
       })
     } else {
       res.status(404).send()
     }
   })
 
-}) 
+})
 
 
 
-app.get('/api/links', (req,res) => {
+app.get('/api/links', (req, res) => {
   conn.query(`SELECT id, url, alias, hitCount FROM urlStore`, (err, rows) => {
     if (err) {
       console.log(err.message);
-      res.status(500).res.json({message: 'internal server error'});
+      res.status(500).json({ message: 'internal server error' });
       return
     }
     res.json({ rows })
   })
 })
+
+
+app.delete('/api/links', (req, res) => {
+  const { id } = req.query;
+  const { secretCode } = req.body;
+
+  conn.query(`SELECT * FROM urlStore;`, (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).res.json({ message: 'internal server error' });
+      return
+    }
+    console.log(id, rows)
+    if (rows.find(element => element.id == id)) {
+      if (rows.find(element => element.secretCode == secretCode)) {
+        conn.query(`DELETE FROM urlStore WHERE secretCode = ${secretCode};`, (err, data) => {
+          if (err) {
+            console.log(err.message);
+            res.status(500).json({ message: 'internal server error' });
+            return
+          }
+          res.status(204).json({ message: `Deleted the url where secretcode: ${secretCode} and id: ${id}` }); // <- ez a json message nem megy át 
+        })
+      } else {
+        res.status(404).json({ message: `The secretCode: ${secretCode} WRONG or doesnt exist` });
+      }
+    } else {
+      res.status(404).json({ message: `The id:${id} doesnt exist` });
+    }
+  })
+});
